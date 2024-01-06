74a6bcb49ecb8c2484e7e93175339592412ef4fc
Shashikant Dwivedi <shashikant.dwivedi.1999@gmail.com>
2024-01-06 13:50:04 +0530

    Added fix for window installation

--- windows/thermal_printer_plugin.cpp
+++ windows/thermal_printer_plugin.cpp
@@ -1,4 +1,4 @@
-#include "include/thermal_printer/thermal_printer.h"
+#include "include/thermal_printer/thermal_printer_plugin.h"
 
 // This must be included before many other Windows headers.
 #include <windows.h>
